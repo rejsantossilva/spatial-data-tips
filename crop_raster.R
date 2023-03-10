a2b09af6d30c5de09fe45341db9aac75a6392174
Rejane Santos-Silva <114105374+rejsantossilva@users.noreply.github.com>
2023-03-10 18:14:03 -0300

    crop_raster.R
    crop_raster.R provides a brief routine to crop and mask raster layer based on polygon using terra package.

---
+++ crop_raster.R
@@ -0,0 +1,65 @@
+# Description -------------------------------------------------------------
+
+
+#This routine imports raster and shapefile files into R, both objects for the 
+#same projection, cuts (and mask) according to the polygon.
+
+
+# Packages ----------------------------------------------------------------
+
+#install, if necessary
+install.packages("terra")
+install.packages("tidyverse")
+install.packages("dplyr")
+
+#load
+library(terra)
+library(tidyverse)
+library(dplyr)
+
+
+# Open file ---------------------------------------------------------------
+
+
+raster <- rast("Path/raster.tif")
+shapefile <- vect("Path/shapefile.shp")
+
+
+# Project shapefile data --------------------------------------------------
+
+
+#First, set current crs
+shapefile <- project(shapefile,
+                     crs(shapefile))
+
+#Now, set a new crs
+shapefile_proj <- project(shapefile,
+                          crs(raster)) #choose the new projection
+
+shapefile_proj
+plot(shapefile_proj)
+
+
+# Crop and mask raster based on polygon -----------------------------------
+
+
+raster_crop <- crop(raster,
+                    ext(shapefile))
+
+raster_crop <- mask(raster_crop,
+                    shapefile)
+
+raster_crop
+plot(raster_crop)
+
+
+# Export raster_crop ------------------------------------------------------
+
+
+setwd("Path")
+writeRaster(x = raster_crop,
+            filename = "raster_crop.tif",
+            overwrite = TRUE)
+
+
+#end
